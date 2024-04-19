987431d00b09b093ef0c4b931682b4b1a6016a41
Lammertyn Pieter-Jan <pjlammertyn@gmail.com>
2024-04-19 14:18:47 +0200

    bumped version

--- build.sh
+++ build.sh
@@ -12,9 +12,9 @@ pip uninstall -y Rabbit-in-a-Blender
 # wait a moment
 sleep 10
 #pip install -i https://test.pypi.org/simple/ Rabbit-in-a-Blender # install from test repo
-pip install --no-cache-dir Rabbit-in-a-Blender==0.0.50
+pip install --no-cache-dir Rabbit-in-a-Blender==0.0.51
 
 
-#git tag -a 0.0.50 -m "0.0.50"
+#git tag -a 0.0.51 -m "0.0.51"
 #git tag
-#git push origin 0.0.50
+#git push origin 0.0.51
