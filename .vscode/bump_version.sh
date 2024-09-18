c5c103e05721a2e6b2ac053ff5ba69e822994d57
Lammertyn Pieter-Jan <pjlammertyn@gmail.com>
2024-09-18 16:18:16 +0200

    renamed file

---
+++ .vscode/bump_version.sh
@@ -0,0 +1,26 @@
+VERSION=0.0.64
+
+#change version in the pyproject.toml
+deactivate 
+pip install toml
+python <<EOF
+import toml
+f = 'pyproject.toml'
+d = toml.load(open(f, 'r'))
+d['project']['version'] = "${VERSION}"
+toml.dump(d, open(f, 'w'))
+EOF
+
+# push the bumped version to git
+git add bump_version.sh
+git add pyproject.toml
+git commit -m "Bumped version to ${VERSION}"
+git push origin
+
+# add tag to git
+git tag -a ${VERSION} -m "${VERSION}"
+git push origin ${VERSION}
+
+# remove a tag local and remote
+# git tag -d ${VERSION}
+# git push origin :refs/tags/${VERSION}
--- .vscode/tag.sh
+++
@@ -1,3 +0,0 @@
-git tag -a 0.0.64 -m "0.0.64"
-#git tag
-git push origin 0.0.64
